df29b0552b5889efe8387bedc6ddbc4cb082ecd7
Danyal Shah <zlac208@live.rhul.ac.uk>
2025-03-13 10:24:11 +0000

    Add Attribute.art

---
+++ runAttribute.bat
@@ -0,0 +1,12 @@
+cls
+javac -cp .;art.jar ARTValuePlugin.java
+java  -cp .;art.jar uk.ac.rhul.cs.csle.art.ART Attribute.art !generate actions
+javac -cp .;art.jar ARTGeneratedActions.java
+java  -cp .;art.jar uk.ac.rhul.cs.csle.art.ART !interpreter attributeAction Attribute.art
+del "*.class"
+
+
+
+
+
+
