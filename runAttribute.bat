84e21d6369da4f74444bf3c86bba1745da516bf9
Danyal Shah <zlac208@live.rhul.ac.uk>
2025-04-09 11:23:01 +0100

    Extract tests to separate tests file

--- runAttribute.bat
+++ runAttribute.bat
@@ -1,9 +1,9 @@
 @echo off
 cls
 javac -cp .;art.jar ARTValuePlugin.java
-java  -cp .;art.jar uk.ac.rhul.cs.csle.art.ART Attribute.art !generate actions
+java  -cp .;art.jar uk.ac.rhul.cs.csle.art.ART Attribute.art !generate actions test.art
 javac -cp .;art.jar ARTGeneratedActions.java
-java  -cp .;art.jar uk.ac.rhul.cs.csle.art.ART !interpreter attributeAction Attribute.art
+java  -cp .;art.jar uk.ac.rhul.cs.csle.art.ART !interpreter attributeAction Attribute.art test.art
 del "*.class"
 
 
